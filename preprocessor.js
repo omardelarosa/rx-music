8b9746d1c7d8dc2df889bf950f75663bfcd5187f
Omar Delarosa <thedelarosa@gmail.com>
2016-01-13 17:19:50 -0500

    0.0.1: added preprocessor.js file

---
+++ preprocessor.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+const _  = require('lodash');
+const path = require('path');
+
+// Set this variable to the directory where your data lives
+const DEFAULT_EVENT_DATA_PATH = '~/Code/downloads/event_data_days/01_12_2016';
+
+
